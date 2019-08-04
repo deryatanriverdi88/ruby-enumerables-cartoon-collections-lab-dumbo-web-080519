067f2d51eb87419cfb926cf6a05ee6b090901983
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-08-04 19:32:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(snacks)
-  # the array below is here to help	  # the array below is here to help
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find do |item|
     cheese_types.include?(item)
